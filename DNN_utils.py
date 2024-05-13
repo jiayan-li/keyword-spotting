cebd277e5186a5017b5fe276ceabc3b3cfed6ba4
emin-ozyoruk <169495604+emin-ozyoruk@users.noreply.github.com>
2024-05-13 12:42:27 -0500

    DNN updates

--- DNN_utils.py
+++ DNN_utils.py
@@ -1,6 +1,8 @@
-def flatten(x):
-    N = x.shape[0] # read in number of samples N
-    return x.view(N, -1)  # "flatten" the other dimensions.
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F  
+
 
 
 
