dd3109ebae045220e7efc399762f298b99a8127f
Jiayan Li <jiayanli@Jiayans-Laptop.local>
2024-05-13 12:48:05 -0500

    fix: states

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ TEST_PHN_PATH = 'timit/data/TRAIN/DR4/MGAG0/SX61.PHN'
 TEST_WAV_PATH = 'timit/data/TRAIN/DR4/MGAG0/SX61.WAV'
 PHONEME_LIST = [
  ('#b', 'n'),
+ ('h#', 'n'),
  ('n',),
  ('n', 'eh'),
  ('eh',),
@@ -9,10 +10,11 @@ PHONEME_LIST = [
  ('v',),
  ('v', 'axr'),
  ('axr',),
+ ('axr', 'h#'),
  ('axr', '#b')]
 
 # Position of phonemes in the label vector. 
 phoneme_to_idx = {'#bn':0, 'h#n', 'n':1, 'neh':2, 'eh':3, 'ehv':4, 
-                  'v':5, 'vaxr':6, 'axr':7, 'axr#b':8, '#b':9, 'h#': 10}
+                  'v':5, 'vaxr':6, 'axr':7, 'axr#b':8, 'axrh#', '#b':9, 'h#': 10}
 
 
