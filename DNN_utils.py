ea67be7fe2f562ed6314044c43993721693f8b78
emin-ozyoruk <169495604+emin-ozyoruk@users.noreply.github.com>
2024-05-13 20:07:25 -0500

    DNN Runs!

--- DNN_utils.py
+++ DNN_utils.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F  
 
+def flatten(x):
+    N = x.shape[0] # read in N, C, H, W
+    return x.view(N, -1)  # "flatten" the C * H * W values into a single vector per image
 
 
 
