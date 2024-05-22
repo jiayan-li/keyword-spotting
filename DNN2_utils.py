b713c4d7e2aaf80bdbee1290037dd1875109cdd2
emin-ozyoruk <169495604+emin-ozyoruk@users.noreply.github.com>
2024-05-22 18:18:18 -0500

    Update: DNN accuracy

--- DNN2_utils.py
+++ DNN2_utils.py
@@ -22,45 +22,8 @@ def column_str_to_numpy(df, colname:str):
     # Given pd.DataFrame df, convert the column colname from string to numpy array.
     if isinstance(df.iloc[0][colname], str):
         df[colname]=df[colname].apply(lambda x: np.fromstring(x[1:-1], sep=' '))
+ 
 
-def check_accuracy(loader, model):
-    if loader.dataset.train:
-        print('Checking accuracy on validation set')
-    else:
-        print('Checking accuracy on test set')
-    num_correct = 0
-    num_samples = 0
-    model.eval()  # set model to evaluation mode
-    with torch.no_grad():
-        for x, y in loader:
-            scores = model(x) 
-            _, preds = scores.max(1) 
-            true_class = y.argmax(dim=1) # True class is the one that has the highest probability in the label vector y.
-            num_correct += (preds == true_class).sum()
-            num_samples += preds.size(0)
-        acc = float(num_correct) / num_samples
-        print('Got %d / %d correct (%.2f)' % (num_correct, num_samples, 100 * acc))
-    return acc 
-
-def check_loss(loader, model):
-    '''
-    Check the loss in a given loader (e.g. validation loss)
-    Args:
-        loader: Loader object for tranining, validation or test sets.
-        model: Our model (e.g. DNN)
-    Returns:
-        loss: Loss calculated with the dataset given by loader (e.g. validation dataset).
-    '''
-    model.eval()
-    total_loss = 0
-    with torch.no_grad():
-        for x, y in loader:
-            scores = model(x)
-            criterion = nn.KLDivLoss(reduction = "batchmean")
-            loss = criterion(F.log_softmax(scores), y) 
-            total_loss += loss    
-    return total_loss
-        
 
 def check_accuracy_12_classes(loader, model):
     if loader.dataset.train:
@@ -107,3 +70,21 @@ def check_accuracy_single_class(loader, model):
     return acc 
 
 
+def check_loss_single_class(loader, model):
+    '''
+    Check the loss in a given loader (e.g. validation loss)
+    Args:
+        loader: Loader object for tranining, validation or test sets.
+        model: Our model (e.g. DNN)
+    Returns:
+        loss: Loss calculated with the dataset given by loader (e.g. validation dataset).
+    '''
+    model.eval()
+    total_loss = 0
+    with torch.no_grad():
+        for x, y in loader:
+            scores = model(x)
+            criterion = nn.CrossEntropyLoss() 
+            loss = criterion(scores, y)  
+            total_loss += loss    
+    return total_loss
